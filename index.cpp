5db5fbd25693fb6140489dd3df0afa83f5303976
Rakesh Vishwakarma <rakesh.tu16@gmail.com>
2021-07-11 12:16:21 +0530

    greeting function implemented

--- index.cpp
+++ index.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
 void welcome() {
@@ -12,10 +13,25 @@ string askForName() {
   return name;
 }
 
+string greeting(string name) {
+  string greet;
+  time_t currTime;
+  currTime = time(NULL);
+
+  tm *tmLocal = localtime(&currTime);
+
+  if((tmLocal->tm_hour > 0) && (tmLocal->tm_hour < 12)) greet = "Morning";
+  else if((tmLocal->tm_hour >= 12) && (tmLocal->tm_hour < 17)) greet = "Afternoon";
+  else if((tmLocal->tm_hour >= 17) && (tmLocal->tm_hour < 20)) greet ="Evening";
+  else greet = "Night";
+
+  greet = greet.append(" " + name + ",");
+  return greet;
+}
+
 int main() {
   welcome();
-  cout<<askForName();
-  // greating();
+  cout<<"Good "<<greeting(askForName())<<endl;
   // transaction();
   return 0;
 }
